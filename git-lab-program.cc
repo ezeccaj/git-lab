7fc9ea665337869bf6d6e2073cbb9bce352b43ab
Emily Jones <ej458917@ohio.edu>
2019-01-22 19:03:25 -0500

    Hello

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *        Author: Emily Jones
+ *        Date: January 22, 2019
+ *        Description: This code prints a messasge to the screen.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
